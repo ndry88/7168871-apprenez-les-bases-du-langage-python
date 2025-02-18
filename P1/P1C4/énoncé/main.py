1d625da2bc9846445795e9b75215e39cd62a7908
ndry88 <ndryfirmin@gmail.com>
2025-02-18 01:48:28 +0100

    Update main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,12 @@
-# Écrivez votre code ici !
+# Création des variables nom et age
+nom = "Ehouman"
+age = 36
+
+# Affichage de la phrase avec les variables
+print (f"Je m'appelle{nom} et j'ai {age} ans.")
+
+# Modification de la variable age
+age = age + 10 
+
+# Affichage de la phrase mise à jour avec les variables
+print (f"Je m'appelle {nom} et j'ai {age} ans maintenant.")
