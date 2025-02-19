198549c5c73630cb081b96c19985f142da53737d
ndry88 <ndryfirmin@gmail.com>
2025-02-19 08:31:01 +0100

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,17 @@
-# Écrivez votre code ici !
+Création des quatre variables
+nom = "EHOUMAN"
+age = 36 
+taille = 1.70
+est_etudiant = True
+
+affichage des valeurs des variables précédemment définies
+print (f"Nom: {nom}")
+print (f"Age: {age}")
+print (f"Taille: {taille}")
+print (f"Est etudiant: {est_etudiant}")
+
+affichage des types des variables précédemment définies
+print (f" Type de Nom: {type (nom)}")
+print (f" Type de Age: {type (age)}")
+print (f" Type de Taille: {type (taille)}")
+print (f" Type de Est etudiant: {type (est_etudiant)}")
