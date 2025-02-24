88a75ac366c7a0000b22d4a1f1df1a056283a120
ndry88 <ndryfirmin@gmail.com>
2025-02-25 00:09:00 +0100

    Update main.py

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,24 @@
-# Écrivez votre code ici !
+# 1.	Créez un dictionnaire appelé  fruits
+fruits={
+  "pomme":"rouge",
+  "banane":"jaune",
+  "orange":"orange"
+}
+print(fruits)
+
+# 2.Ajoutez la clé  kiwi  avec la valeur  vert  au dictionnaire fruits  
+fruits['kiwi'] = "vert"
+print(fruits)
+
+#3.	Accédez à la valeur correspondant à la clé  banane  et stockez-la dans une variable appelée  couleur_banane =
+couleur_banane= fruits["banane"]
+print (couleur_banane)
+
+#4.	Modifiez la valeur associée à la clé pomme  pour  vert  
+fruits["pomme"]="vert"
+
+#5.	Supprimez la clé  banane  du dictionnaire  fruits  
+del fruits["banane"]
+
+#6.	Affichez les clés restantes dans le dictionnaire
+print(fruits.keys())
