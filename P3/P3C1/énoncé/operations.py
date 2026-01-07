9bb1ab406d4c750f16bfa770b3b72e17fb0990a5
ndry88 <ndryfirmin@gmail.com>
2026-01-07 17:12:03 +0100

    Update operations.py

--- P3/P3C1/énoncé/operations.py
+++ P3/P3C1/énoncé/operations.py
@@ -1,1 +1,14 @@
-# Écrivez votre code ici !
+Dans le fichier `operations.py` , on peut définir les fonctions addition et multiplication
+def adition (a,b):
+  return a + b
+def multitiplication (a * b):
+  return a * b 
+Dans le fichier principal, on peut importer les fonctions de `operations.py`
+# à l'aide de l'instruction `import` , puis les utiliser pour effectuer des opérations.
+import operations
+# Utilisation de la fonction addition
+resultat_addition = operations.addition (3,5)
+print("Résultat de l'addition :", resultat_addition)
+# Utilisation de la fonction multiplication
+resultat_multiplication = operations.multiplication(8, 2)
+print("Résultat de la multiplication :", resultat_multiplication)
